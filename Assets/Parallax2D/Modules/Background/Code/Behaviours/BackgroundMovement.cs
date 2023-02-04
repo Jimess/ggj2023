d7cfbc1aaad4a218be6a542a123ca7c586a48d3a
Laurynas P <laurynas.pupsta@gmail.com>
2023-02-04 17:04:09 +0200

    many things

---
+++ Assets/Parallax2D/Modules/Background/Code/Behaviours/BackgroundMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Parallax2D.Modules.Background.Code.Behaviours
+{
+  public class BackgroundMovement : MonoBehaviour
+  {
+    public Vector2 Speed;
+    public List<CheckPosition> CheckPosition = new List<CheckPosition>();
+    private Transform player;
+
+    public void Construct(Transform player, bool nineImage)
+    {
+      this.player = player;
+      transform.position = player.position;
+
+      foreach (var checkInBound in CheckPosition)
+        checkInBound.Construct(player, nineImage);
+    }
+
+    public void UpdateMovement()
+    {
+      var position = new Vector3(player.position.x, player.position.y, 0);
+            transform.position = new Vector3(position.x * Speed.x, position.y * Speed.y, position.z);
+      
+      foreach (var checkPosition in CheckPosition)
+        checkPosition.UpdateStatus();
+    }
+  }
+}
