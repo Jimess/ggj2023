6c1aadacf41ff0c0a2d0758d71577d8677cf6436
Laurynas P <laurynas.pupsta@gmail.com>
2023-02-04 23:46:46 +0200

    end tree changes

--- Assets/AcornDisabler.cs
+++ Assets/AcornDisabler.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class AcornDisabler : MonoBehaviour
 {
+    private bool isEnd = false;
+    private bool lastTouch = false;
+
     private void OnEnable()
     {
         LevelController.OnEnd += DisableAcorn;
@@ -14,6 +17,8 @@ public class AcornDisabler : MonoBehaviour
     {
         LevelController.OnEnd -= DisableAcorn;
         LevelController.OnStart -= EnableAcorn;
+        CollisionManager.OnAcornTrackCollision -= RegisterLastTouch;
+        CollisionManager.OnAcornDamageTaken -= RegisterLastTouch;
     }
 
     // Start is called before the first frame update
@@ -34,6 +39,12 @@ public class AcornDisabler : MonoBehaviour
     {
         GetComponent<AcornRotator>().enabled = false;
         GetComponent<AcornJumper>().enabled = false;
+        if (win)
+        {
+            StartCoroutine(WaitForLastTouch());
+            CollisionManager.OnAcornTrackCollision += RegisterLastTouch;
+            CollisionManager.OnAcornDamageTaken += RegisterLastTouch;
+        }
     }
 
     private void EnableAcorn()
@@ -41,5 +52,35 @@ public class AcornDisabler : MonoBehaviour
         GetComponent<Rigidbody2D>().isKinematic = false;
         GetComponent<AcornRotator>().enabled = true;
         GetComponent<AcornJumper>().enabled = true;
+    }
+
+    IEnumerator WaitForLastTouch()
+    {
+        while (!lastTouch)
+        {
+            yield return null;
+        }
+        GetComponent<Rigidbody2D>().isKinematic = true;
+        Collider2D[] cols = GetComponentsInChildren<Collider2D>();
+        LevelController.Instance.SummonTree(transform.position);
+
+        foreach (Collider2D col in cols)
+        {
+            col.enabled = false;
+        }
+    }
+
+    private void RegisterLastTouch(float bounceForce, Collision2D collision2D)
+    {
+        lastTouch = true;
+        CollisionManager.OnAcornTrackCollision -= RegisterLastTouch;
+        CollisionManager.OnAcornDamageTaken -= RegisterLastTouch;
+    }
+
+    private void RegisterLastTouch()
+    {
+        lastTouch = true;
+        CollisionManager.OnAcornTrackCollision -= RegisterLastTouch;
+        CollisionManager.OnAcornDamageTaken -= RegisterLastTouch;
     }
 }
--- Assets/TreeGrow.cs
+++ Assets/TreeGrow.cs
@@ -11,18 +11,18 @@ public class TreeGrow : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.B))
-        {
-            Grow(transform.position);
-        }
+        //if (Input.GetKeyDown(KeyCode.B))
+        //{
+        //    Grow(transform.position);
+        //}
     }
 
-    private void Grow(Vector2 position)
+    public void Grow(Vector2 position)
     {
         Sequence seq = DOTween.Sequence();
-        seq.Append(gameObject.transform.DOScale(1f, 0.5f).From(0f).SetEase(Ease.OutQuint));
+        seq.Append(gameObject.transform.DOScale(1f, 1.5f).From(0f).SetEase(Ease.OutQuint));
         //seq.Append(gameObject.transform.DOBlendableRotateBy(Vector3.forward * 30f, 0.25f).SetLoops(1, LoopType.Yoyo));
-        seq.Join(gameObject.transform.DOShakeRotation(0.5f, str, vibrato, randomness, false, ShakeRandomnessMode.Full));
+        seq.Join(gameObject.transform.DOShakeRotation(1.5f, str, vibrato, randomness, false, ShakeRandomnessMode.Full));
         //seq.Join(gameObject.transform.DOSh)
     }
 }
